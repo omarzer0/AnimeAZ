e4b87cb7c82dfdc34613aad6ad0f01fdae1ee795
Omar Adel <omaradel@MBP-von-Felix.fritz.box>
2023-09-27 16:03:02 +0200

    Add composables

---
+++ shared/src/commonMain/kotlin/az/zero/animeaz/presentation/shared/LoadingComposable.kt
@@ -0,0 +1,22 @@
+package az.zero.animeaz.presentation.shared
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun LoadingComposable(
+    modifier: Modifier = Modifier,
+    color: Color = Color.Magenta
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(color = color)
+    }
+}
