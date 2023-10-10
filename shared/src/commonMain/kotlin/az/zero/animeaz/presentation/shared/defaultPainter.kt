1bc2267c95104289b2e1809ceea65ad433a976af
omarzer0 <omarzero1015@gmail.com>
2023-10-10 13:26:53 +0200

    Update painterResource issue comment

--- shared/src/commonMain/kotlin/az/zero/animeaz/presentation/shared/defaultPainter.kt
+++ shared/src/commonMain/kotlin/az/zero/animeaz/presentation/shared/defaultPainter.kt
@@ -9,16 +9,16 @@ import dev.icerock.moko.resources.compose.painterResource
 @Composable
 fun rememberDefaultPainter(url: String): Painter {
     // FIXME **IMPORTANT** for some reason painterResource() from moko
-    //  library doesn't work anymore for the IOS aoo although it worked before
+    //  library sometimes doesn't work for the IOS
     //  try to solve it as soon as possible
     //  maybe it is a problem of coping resources to ios part
     return rememberImagePainter(
         url,
-//        placeholderPainter = {
-//            painterResource(SharedRes.images.placeholder)
-//        },
-//        errorPainter = {
-//            painterResource(SharedRes.images.no_image)
-//        }
+        placeholderPainter = {
+            painterResource(SharedRes.images.placeholder)
+        },
+        errorPainter = {
+            painterResource(SharedRes.images.no_image)
+        }
     )
 }
